f857d21827366bcff57fb0801128896cf53b31bb
a-mariam <mariiam22222@gmail.com>
2024-01-24 10:37:16 +0100

    conflict

--- src/main/java/bluma/africa/blumaafrica/service/UserService.java
+++ src/main/java/bluma/africa/blumaafrica/service/UserService.java
@@ -14,7 +14,11 @@ import bluma.africa.blumaafrica.exceptions.UserNotFound;
 import bluma.africa.blumaafrica.dtos.requests.*;
 import bluma.africa.blumaafrica.dtos.responses.*;
 import bluma.africa.blumaafrica.exceptions.*;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+
+@Component
 public interface UserService {
     UserResponse createUser(UserRequest request) throws UserAlreadyExist, UserNotFound, EmailException;
     User getUserBy(String username);
